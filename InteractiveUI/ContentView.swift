791e9fac660cc955d667195608984553ccf596fd
Eleanor Abitbol <eleanor.abitbol@gmail.com>
2025-08-06 15:11:53 -0400

    added alert(not done yet)  and added a second user input

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,20 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @state private var presentAlert = fasle
+    
    @State private var name = ""
-    @State private var textTitle = "What is your name?"
+    @State private var age = ""
+    @State private var textTitle = "What is your name and age?"
     var body: some View {
         VStack {
+     
             Text(textTitle)
                 .font(.title)
             TextField("Type Your Name Here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
-            Button("Submit Name") {
-               textTitle="Welcome, \(name)!"
+            TextField("Type Your Age Here...", text: $age)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.mint, width: 1)
+            Button("Submit Name and Age") {
+               textTitle="Welcome, \(name). You are \(age) years old!"
                 name=""
+                Text(presentAlert ? "Presenting":"Dismissed")
+                Button("Alert") = true
             }//Button Closing
+            
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.pink)
