6b49dacaad8f9484c55ab44f829d6002aef486e1
Eleanor Abitbol <eleanor.abitbol@gmail.com>
2025-08-06 14:38:52 -0400

    Added button and modifyers

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,7 +12,17 @@ struct ContentView: View {
         VStack {
             Text("What is your name?")
                 .font(.title)
-        }
+            TextField("Type Your Name Here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }//Button Closing
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }//Vstack Closing
         .padding()
     }
 }
